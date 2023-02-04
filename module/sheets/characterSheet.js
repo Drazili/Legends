bfb2908dd9371693c18ce9de1177da584cb4505e
Sebastian Svensson Thaning <ssth.skp@edu.mercantec.dk>
2023-02-04 14:18:41 +0100

    Added rollability to the backend of things.

--- module/sheets/characterSheet.js
+++ module/sheets/characterSheet.js
@@ -6,5 +6,34 @@ export default class characterSheet extends ActorSheet{
     activateListeners(html){
         super.activateListeners(html)
         
+        html.find('.rollable').click(this._onRoll.bind(this));
     }
+
+    _onRoll(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        const dataset = element.dataset;
+    
+        // Handle item rolls.
+        if (dataset.rollType) {
+          if (dataset.rollType == 'item') {
+            const itemId = element.closest('.item').dataset.itemId;
+            const item = this.actor.items.get(itemId);
+            if (item) return item.roll();
+          }
+        }
+    
+        // Handle rolls that supply the formula directly.
+        if (dataset.roll) {
+          let label = dataset.label ? `${dataset.label}` : '';
+          let roll = new Roll(dataset.roll, this.actor.getRollData());
+          roll.toMessage({
+            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            flavor: label,
+            rollMode: game.settings.get('core', 'rollMode'),
+          });
+          return roll;
+        }
+      }
+    
 }
