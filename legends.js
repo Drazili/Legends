fc8fd628fb1871cb4577225b16ba10de51fdbe59
Sebastian Svensson Thaning <ssth.skp@edu.mercantec.dk>
2024-09-12 13:49:00 +0200

    Added item control and item sheets

--- legends.js
+++ legends.js
@@ -1,6 +1,8 @@
 import itemSheet from "./module/sheets/itemSheet.js";
 import character from "./module/documents/character.js";
 import characterSheet from "./module/sheets/characterSheet.js";
+import LegendsItem from "./module/documents/item.js";
+import LegendsCharacter from "./module/documents/character.js";
 
 // Hooks.on("renderCombatTracker", (app, html, data) => {
 //     console.log("!!!!!!");
@@ -11,13 +13,15 @@ Hooks.once("init", function(){
     console.log("Legends | Initializing legends");
 
     CONFIG.Combat.initiative = {
-        formula: "1d20 + @attributes.initiative.value"
+        formula: "1d20 + @attributes.initiative.max"
     };    
+
+    CONFIG.Item.documentClass = LegendsItem;
 
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("legends", itemSheet, {makeDefault: true});
 
-    CONFIG.Actor.documentClass = character;
+    CONFIG.Actor.documentClass = LegendsCharacter;
 
     Actors.unregisterSheet("core", ActorSheet);
     Actors.registerSheet("legends", characterSheet, {makeDefault: true});
